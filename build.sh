820b2b4f34659cbebb09fdab8ca6a2268c0cceed
Ed <128392461+edtheanchor@users.noreply.github.com>
2024-09-26 16:49:10 +0100

    switch to bun. improve build script

--- build.sh
+++ build.sh
@@ -3,13 +3,15 @@
 echo "✨ Starting new build..."
 echo "🚫 Clearing existing build..."
 rm -rf dist
+echo "🚫 Removing lock & build files..."
+rm -rf yarn.lock && rm -rf package-lock.json && rm -rf bun.lockb && rm -rf tsconfig.tsbuildinfo
 echo "🚫 Removing node_modules..."
 rm -rf node_modules
 echo "Checking for git updates..."
 git pull
-echo "Installing yarn modules..."
-yarn install
+echo "Installing modules..."
+bun install
 echo "Building app..."
-yarn run build:tsc
+bun run build:tsc
 echo "Restarting server..."
 pm2 restart all
